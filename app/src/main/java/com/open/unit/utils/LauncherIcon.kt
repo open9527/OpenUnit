a173bd2e632a197da40a4d4d6729a93b38aee591
open9527 <open_9527@163.com>
2023-12-11 14:13:00 +0800

    1.Fix 动态切换icon

--- app/src/main/java/com/open/unit/utils/LauncherIcon.kt
+++ app/src/main/java/com/open/unit/utils/LauncherIcon.kt
@@ -5,7 +5,11 @@ import android.content.pm.PackageManager
 import com.open.core.ContextHolder
 
 object LauncherIcon {
-
+    /**
+     * 1.配置icon的activity 直接继承SplashActivity,
+     * 2.配置icon的activity要把 enabled 属性设为false(否则出现多个icon),配置指定icon属性
+     * 3.切换最佳时机是监听app切换到后台触发变更icon
+     */
     fun switchIcon(enabledCls: Class<*>, disabledCls: Class<*>) {
         val packageManager = ContextHolder.get().packageManager
         packageManager.setComponentEnabledSetting(
